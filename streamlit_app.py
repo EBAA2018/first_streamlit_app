9d9c77bce7e18ca41a374b3c116d0e23a2535d2d
EBAA2018 <129955898+EBAA2018@users.noreply.github.com>
2023-04-05 13:53:47 +0200

    added new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 2 & Blueberry Oatmeal') 
+streamlit.text('Kale, Spinach & Pocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
