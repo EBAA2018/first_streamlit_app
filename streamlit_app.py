7344dd4cd79ebe0a35f9c60294e6795b48bb88d3
EBAA2018 <129955898+EBAA2018@users.noreply.github.com>
2023-04-18 10:32:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ except URLError as e:
 #streamlit.stop()
 
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("The fruit load list containsss:")
 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
